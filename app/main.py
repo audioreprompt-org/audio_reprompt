18980716304bd19f29716565bd873636e5552056
Jose082 <jose.garcia@universehorizons.com>
2025-11-08 00:37:44 -0500

    Update configurations

--- app/main.py
+++ app/main.py
@@ -1,24 +1,30 @@
 from contextlib import asynccontextmanager
 
+from typing import cast
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.datastructures import State
 
 from app.clients.s3 import S3Client
 from app.routes import routes
+from config import get_environment_config
 
 from models.musicgen.model import load_musicgen_pipeline
 
 
+ENV_CFG = get_environment_config()
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    state = cast(State, app.state)
     # TODO: Take the bucket and region for the config
-    app.state.s3 = S3Client(bucket="audio_generations", region="us-west-2")
-    app.state.synthesizer = load_musicgen_pipeline()
+    state.s3 = S3Client(bucket=ENV_CFG.bucket_name, region=ENV_CFG.aws_region)
+    state.synthesizer = load_musicgen_pipeline()
 
     yield
 
 
-app = FastAPI(title='audio_prompt')
+app = FastAPI(title='audio_prompt', lifespan=lifespan)
 
 
 app.add_middleware(
